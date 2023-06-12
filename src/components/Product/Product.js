41048bd1be716ee645d0fb23c3a3094c061885b0
Michal Sak <michusakowicz@gmail.com>
2023-06-12 10:53:17 +0200

    Edit getPrice - useMemo

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,19 +1,19 @@
 import styles from './Product.module.scss';
-// import productsData from '../../data/products';
-import React from 'react';
 import ProductForm from './ProductForm/ProductForm';
-// import PropTypes from 'prop-types';
-import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useState, useMemo } from 'react';
+
+
 const Product = ({  name, title, basePrice, colors, sizes }) => {
 
   const [currentColor, setCurrentColor] = useState(colors[0]);
   const [currentSize, setCurrentSize] = useState(sizes[0].name);
 
 
-  const getPrice = () => {
+  const getPrice = useMemo(() => {
     const foundSize = sizes.find((size) => size.name === currentSize);
     return basePrice + foundSize.additionalPrice;
-  };
+  }, [basePrice, currentSize, sizes]);
 
   const addToCart = (e) => {
     e.preventDefault();
@@ -39,7 +39,7 @@ const Product = ({  name, title, basePrice, colors, sizes }) => {
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {getPrice}$</span>
         </header>
         <ProductForm
           sizes={sizes}
@@ -55,19 +55,17 @@ const Product = ({  name, title, basePrice, colors, sizes }) => {
   )
 };
 
-// Product.propTypes = {
-
-//   id: PropTypes.number.isRequired,
-//   name: PropTypes.string.isRequired,
-//   title: PropTypes.string.isRequired,
-//   basePrice: PropTypes.number.isRequired,
-//   colors: PropTypes.arrayOf(PropTypes.string).isRequired,
-//   sizes: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       name: PropTypes.string,
-//       additionalPrice: PropTypes.number,
-//     })
-//   ).isRequired,
-// };
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  sizes: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      additionalPrice: PropTypes.number,
+    })
+  ).isRequired,
+};
 
 export default Product;
